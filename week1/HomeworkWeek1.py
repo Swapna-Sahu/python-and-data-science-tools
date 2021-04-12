a9f066ad755408b60f49ff1bb10ab7a9d1ebd02d
Swapna <swapna.sahu85@gmail.com>
2021-04-12 16:12:25 +0200

    updated homework

--- week1/HomeworkWeek1.py
+++ week1/HomeworkWeek1.py
@@ -3,7 +3,7 @@
 name = input("Enter your name")
 age = int(input("Enter your age"))
 
-print("Your name is : ", name, " Your age is : ", age)
+print(f"Your name is : {name}. Your age is : {age}")
 
 # Task 2 - Create a list of your 5 favorite movies and store it in the variable
 
@@ -29,36 +29,40 @@ name = input("enter your name")
 weight = float(input("enter your weight in kg"))
 height = float(input("enter your height in meter"))
 
-bmi = weight/height*height
+bmi = weight/height**2
 print(name, "'s BMI is ", bmi)
 
 # Task 6 - Guess a number game - between 1 to 9.
 
-guessnumber = 7
+guess_number = 7
 while True:
-    userguess = int(input("Enter your guess number"))
-    if userguess == guessnumber:
+    user_guess = int(input("Enter your guess number"))
+    if user_guess == guess_number:
         print("Well guessed!")
         break
 
 # Task 7 - Create a tuple with different data types
 
-tuple1 = ("hello world", 134, True, 40.4)
+sample_tuple = ("hello world", 134, True, 40.4)
 
-print(tuple1)
+print(sample_tuple)
 
 # Task 8 - Create a list of 5 city names and convert it into tuples.
 
 city = ["Delhi", "Mumbai", "Kolkata", "Chennai", "Copenhagen"]
 print("Type of city variable : ", type(city))
-citytuple = tuple(city)
-print("Type of citytuple variable : ", type(citytuple))
-print("City Tuple : ", citytuple)
+city_tuple = tuple(city)
+print("Type of city_tuple variable : ", type(city_tuple))
+print("City Tuple : ", city_tuple)
 
 # Task 9 - Remove duplicated from the list
-a = [10, 20, 30, 20, 10, 50, 60, 40, 80, 50, 40]
-a = list(dict.fromkeys(a))
-print(a)
+sample_list = [10, 20, 30, 20, 10, 50, 60, 40, 80, 50, 40]
+sample_list = list(dict.fromkeys(sample_list))
+print(sample_list)
+
+example_list = [10, 20, 30, 20, 10, 50, 60, 40, 80, 50, 40]
+example_list = list(set(example_list))
+print(example_list)
 
 # Task 10 - Accept a string from user and remove the characters which have odd index values of a given string and print them.
 
@@ -66,9 +70,12 @@ str1 = input("Please Enter your Own String : ")
 
 str2 = ''
 
-for i in range(len(str1)):
-    if (i % 2 == 0):
-        str2 = str2 + str1[i]
+# for i in range(len(str1)):
+#    if (i % 2 == 0):
+#        str2 += str1[i]
+for i, sample_int in enumerate(str1):
+    if i % 2 == 0:
+        str2 += str1[i]
 
 print("Original String :  ", str1)
 print("Final String :     ", str2)
